79b4a5f01e45f192d26f626d817a1aff75540151
TejasPrabhu <tejas.prabhu29@gmail.com>
2023-06-25 15:13:23 -0400

    Update react src code with correct urls

---
+++ transcoderx-frontend/src/App.js
@@ -0,0 +1,15 @@
+import { LoginPage } from './pages/LoginPage';
+import UploadPage from './pages/UploadPage';
+import DownloadPage from './pages/DownloadPage';
+
+function App() {
+	return (
+		<>
+			<LoginPage />;
+			<UploadPage />;
+			<DownloadPage />;
+		</>
+	);
+}
+
+export default App;
